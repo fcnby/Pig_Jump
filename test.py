b1dbacf88c9f1cce73e2d721a9513f06e059c8f6
kiril <kdibragimov@edu.hse.ru>
2021-12-18 11:32:27 +0300

    test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from controls import max_score
+from controls import overwriting_max_score
 
 class GameTests(unittest.TestCase):
 
@@ -13,6 +14,21 @@ class GameTests(unittest.TestCase):
         g.write(str('0'))
         g.close()
         self.assertEqual(max_score(),0)
+    def test_MaxScore1(self):
+        max_score = overwriting_max_score(0,15)
+        self.assertEqual(max_score,15)
+
+    def test_MaxScore2(self):
+        max_score = overwriting_max_score(16,15)
+        self.assertEqual(max_score,16)
+
+    def test_overwriting_max_score(self):
+        max_score = overwriting_max_score(10,15)
+        g = open('bestresult.txt', 'w+')
+        f = g.readline()
+        g.close()
+        print(f,'')
+
 
 if __name__ == "__main__":
     unittest.main()
